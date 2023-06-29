999d9f00707102cb25bc828c873da2368fdec800
Lucas Jensch <92726945+lucasjensch@users.noreply.github.com>
2023-06-29 12:19:18 +0200

    Add files via upload

---
+++ website-setup.bat
@@ -0,0 +1,12 @@
+@echo off
+
+md newproject
+
+mkdir "newproject/css"
+mkdir "newproject/js"
+mkdir "newproject/img"
+
+echo >newproject\css\style.css
+echo >newproject\index.html
+echo >newproject\js\main.js
+
